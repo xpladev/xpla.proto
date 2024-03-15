b83e07224bab6d741527bc81d4019a61aee7898d
woosweb <woosweb@gmail.com>
2024-03-15 10:18:01 +0900

    fix: remove legacy code

--- js/scripts/proto-gen.sh
+++ js/scripts/proto-gen.sh
@@ -15,8 +15,12 @@ COSMOS_THIRD_PARTY_DIR="../cosmos-sdk/third_party/proto"
 COSMWASM_DIR="../cosmwasm/proto"
 ETHER_DIR="../ethermint/proto"
 EVMOS_DIR="../evmos/proto"
+IBC_DIR="../ibc/proto"
+IBC_THIRD_PARTY_DIR="../ibc/third_party/proto"
 XPLA_DIR="../xpla/proto"
 
+# ibc 모듈이 요구하는 proofs.proto를 만족시키기 위한 hack
+mv $COSMOS_THIRD_PARTY_DIR/confio/proofs.proto ../
 protoc \
   --plugin="protoc-gen-ts_proto=${PROTOC_GEN_TS_PROTO_PATH}" \
   --ts_proto_out="${OUT_DIR}" \
@@ -26,5 +30,11 @@ protoc \
   --proto_path="$COSMWASM_DIR" \
   --proto_path="$ETHER_DIR" \
   --proto_path="$EVMOS_DIR" \
+  --proto_path="$IBC_DIR" \
+  --proto_path="$IBC_THIRD_PARTY_DIR" \
   --proto_path="$XPLA_DIR" \
-  $(find ${COSMOS_DIR} ${COSMOS_THIRD_PARTY_DIR} ${COSMWASM_DIR} ${ETHER_DIR} ${EVMOS_DIR} ${XPLA_DIR} -path -prune -o -name '*.proto' -print0 | xargs -0)
+  $(find ${COSMOS_THIRD_PARTY_DIR} -path -prune -o -name '*.proto' -print0 | xargs -0) \
+  $IBC_THIRD_PARTY_DIR/proofs.proto \
+  $(find ${COSMOS_DIR} ${COSMWASM_DIR} ${ETHER_DIR} ${EVMOS_DIR} ${IBC_DIR} ${XPLA_DIR} -path -prune -o -name '*.proto' -print0 | xargs -0)
+mv ../proofs.proto $COSMOS_THIRD_PARTY_DIR/confio/
+
